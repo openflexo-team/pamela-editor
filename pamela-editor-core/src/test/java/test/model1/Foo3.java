f370349be04efe0e361b3cb07644123753987aa8
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2026-06-12 11:05:18 +0200

    LOW / Test class

--- pamela-editor-core/src/test/java/test/model1/Foo3.java
+++ pamela-editor-core/src/test/java/test/model1/Foo3.java
@@ -2,6 +2,11 @@ package test.model1;
 
 
 public interface Foo3 {
-
+	
 	// This is not a PAMELA entity
+
+	static class Foo3Impl implements Foo3 {
+		private int anInt;
+		protected String aString;
+	}
 }
